bd3ef4c0f7d3b7b9d0c1142aaef582aaac3f9343
selestinjohn <166312025+selestinjohn@users.noreply.github.com>
2025-10-16 16:44:41 +0300

    Configured Celery to use Upstash Redis

--- PG-Rent-Reminder-Web-Service-master/pg/settings.py
+++ PG-Rent-Reminder-Web-Service-master/pg/settings.py
@@ -130,8 +130,8 @@ BEEM_SECRET_KEY = os.getenv("BEEM_SECRET_KEY")
 
 
 # Celery
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+CELERY_BROKER_URL =  os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 CELERY_TIMEZONE = 'Africa/Dar_es_Salaam'
 CELERY_ENABLE_UTC = False
 
