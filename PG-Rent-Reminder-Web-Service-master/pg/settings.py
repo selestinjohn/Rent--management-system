47cbcc5a1ef7bcee50b1507245b9939099066f53
selestinjohn <166312025+selestinjohn@users.noreply.github.com>
2025-10-17 17:34:30 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- PG-Rent-Reminder-Web-Service-master/pg/settings.py
+++ PG-Rent-Reminder-Web-Service-master/pg/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['rent-management-system-4mh7.onrender.com', 'localhost', '127.0.0.1']
 
 
 
